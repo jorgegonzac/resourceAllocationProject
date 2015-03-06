d2070844da4fd97118a9f0b4998171001308d15c
AntonioEsperC <a01202743@itesm.mx>
2015-03-06 13:20:03 -0600

    Bootstrap Included

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,4 +1,7 @@
 <!-- ARCHIVO PARA SCRIPTS JQUERY E INCLUDES DE BOOTSTRAP ETC ETC -->
 <link rel="icon" href="favicon.jpg"/>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!-- CSS are placed here -->
+{{ HTML::style('css/bootstrap.css') }}
+{{ HTML::style('css/bootstrap-theme.css') }}
 <script></script>
--- app/views/resources.blade.php
+++ app/views/resources.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.layout')
 @section('content')
-	
+	<!-- NAVEGACIÓN DEL USUARIO
+		1.- SELECT DE LOS LABORATORIOS (CON BASE DE DATOS)
+		2.- SEARCH BAR
+	-->
+	<div class="row">
+		<!-- SELECT LABS -->
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">Labs</div>
+		<!-- SEARCH BAR -->
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">Selects</div>
+	</div>
 @stop
