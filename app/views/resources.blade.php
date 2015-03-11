9db473b7a1b0cb806d6e348f6e87ef768f142397
AntonioEsperC <a01202743@itesm.mx>
2015-03-11 11:50:59 -0600

    DistinctWorking

--- app/views/resources.blade.php
+++ app/views/resources.blade.php
@@ -25,35 +25,31 @@
 	<div class="row">
 		<div class="resources">
 			<h1>RECENT RESOURCES</h1>
-			@foreach($recentBookings as $recentBooking)
+			@foreach( $recentBookings as $recentBooking)
 				<div class="col-sm-6 col-md-3">
 					<div class="panel panel-default panel-card">
-                    <div class="panel-heading">
-                        <img src="{{$recentBooking->resource->image}}" style="width:200px;height:228px"/>
-                        <button class="btn btn-primary btn-sm" role="button">Apartar</button>
-                    </div>
-                    <div class="panel-body text-center">
-                        <h4 class="panel-header"><a href="">{{$recentBooking->resource->name}}</a></h4>
-                        <small>{{$recentBooking->resource->description}}</small>
-                    </div>
-                    <div class="panel-thumbnails">
-                        <div class="row">
-                            <div class="col-xs-4">
-                            	<img src="{{$recentBooking->resource->image}}" style="width:200px;height:228px"/>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-					<!-- <h1>
-						<a href="">
-							<p>{{$recentBooking->resource->name}}</p>
-							 <img src="{{$recentBooking->resource->image}}" alt="{{$recentBooking->resource->name}}" style="width:304px;height:228px">
-						</a>
-					</h1> -->
+	                    <div class="panel-heading">
+	                        <img src="{{$recentBooking->image}}" style="width:200px;height:228px"/>
+	                        <button class="btn btn-primary btn-sm" role="button">Apartar</button>
+	                    </div>
+	                    <div class="panel-body text-center">
+	                        <h4 class="panel-header">
+	                        	<a href="">
+	                        		{{$recentBooking->name}}
+	                        	</a>
+	                        </h4>
+	                        <small>{{$recentBooking->description}}</small>
+	                    </div>
+	                    <div class="panel-thumbnails">
+	                        <div class="row">
+	                            <div class="col-xs-4">
+	                            	<img src="{{$recentBooking->image}}" style="width:200px;height:228px"/>
+	                            </div>
+	                        </div>
+	                    </div>
+                	</div>
 				</div>
-				
-		@endforeach
+			@endforeach
 		</div>
 	</div>
 @stop
