5c6ba6a76e204fc9f1f2cc1f38ca9ed19c69e6b7
a01320622 <a01320622@itesm.mx>
2015-03-30 09:33:43 -0600

    Nothing
    NOthing

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Intervention\Image\ImageServiceProvider'
+		// 'Intervention\Image\ImageServiceProvider'
 
 	),
 
@@ -189,7 +189,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Image'           => 'Interventation\Image\Facades\Image'
+		// 'Image'           => 'Interventation\Image\Facades\Image'
 
 	),
 
