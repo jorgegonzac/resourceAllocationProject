dc9c817d09ded9ce3af0a23e1d917a42dcb602f7
a01320622 <a01320622@itesm.mx>
2015-03-30 09:09:17 -0600

    how resources look
    have to resize images somehow

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -5,6 +5,12 @@
 <!-- CSS are placed here -->
 {{ HTML::style('css/bootstrap.css') }}
 {{ HTML::style('css/bootstrap-theme.css') }}
+{{ HTML::style('css/component.css')}}
+{{ HTML::style('css/demo.css')}}
+{{ HTML::style('css/normalize.css')}}
+{{ HTML::script('js/hover.js') }}
+{{ HTML::script('js/snap.svg-min.js') }}
+
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
@@ -22,29 +28,7 @@
 				$('.resources').empty();
 				$('.resources').append('<br><br>');
 				$.each(data, function(key, value){
-		    		$(".resources").append('<div class="col-sm-6 col-md-3">
-					<div class="panel panel-default panel-card">
-	                    <div class="panel-heading">
-	                        <img src="'+value.image+'" style="width:200px;height:228px"/>
-	                        <button class="btn btn-primary btn-sm" role="button">Apartar</button>
-	                    </div>
-	                    <div class="panel-body text-center">
-	                        <h4 class="panel-header">
-	                        	<a href="">'+
-	                        		value.name+
-	                        	'</a>
-	                        </h4>
-	                        <small>'+value.description+'</small>
-	                    </div>
-	                    <div class="panel-thumbnails">
-	                        <div class="row">
-	                            <div class="col-xs-4">
-	                            	<img src="'+value.image+'" style="width:200px;height:228px"/>
-	                            </div>
-	                        </div>
-	                    </div>
-                	</div>
-				</div>');
+		    		$(".resources").append('<div class="col-sm-6 col-md-3"><div class="panel panel-default panel-card"><div class="panel-heading"><img src="'+value.image+'" style="width:200px;height:228px"/><button class="btn btn-primary btn-sm" role="button">Apartar</button></div><div class="panel-body text-center"><h4 class="panel-header"><a href="">'+value.name+'</a></h4><small>'+value.description+'</small></div><div class="panel-thumbnails"><div class="row"><div class="col-xs-4"><img src="'+value.image+'" style="width:200px;height:228px"/></div></div></div></div></div>');
 		    	});
 			});
 		});
--- app/views/resources.blade.php
+++ app/views/resources.blade.php
@@ -22,16 +22,33 @@
 		</div>
 	</div>
 	
-	<div class="row">
 		<div class="resources">
+			<section id="grid" class="grid clearfix">
 			<h1>RECENT RESOURCES</h1>
 			@foreach( $recentBookings as $recentBooking)
-				<div class="col-sm-6 col-md-3">
+
+					
+					<a href="" data-path-hover="M 0,0 0,38 90,58 180.5,38 180,0 z">
+						<figure>
+							<img src="{{$recentBooking->image}}" style="width:250px;height:437px" />
+							<svg viewBox="0 0 180 320" preserveAspectRatio="none"><path d="M 0 0 L 0 182 L 90 126.5 L 180 182 L 180 0 L 0 0 z "/></svg>
+							<figcaption>
+								<h2>{{$recentBooking->name}}</h2>
+								<p>{{$recentBooking->description}}</p>
+								<button>View</button>
+							</figcaption>
+						</figure>
+					</a>
+					
+				<!-- <div class="col-sm-6 col-md-3">
 					<div class="panel panel-default panel-card">
-	                    <div class="panel-heading">
-	                        <img src="{{$recentBooking->image}}" style="width:200px;height:228px"/>
-	                        <button class="btn btn-primary btn-sm" role="button">Apartar</button>
+	                    <div class="panel-heading"> -->
+	                       <!--  <img src="{{$recentBooking->image}}" style="width:200px;height:228px"/> -->
+	                      <!--   <button class="btn btn-primary btn-sm" role="button">Apartar</button>
 	                    </div>
+	                     <div class="panel-figure">
+                        <img class="img-responsive img-circle" src="{{$recentBooking->image}}" style="width:150px;height:150px" />
+                    	</div>
 	                    <div class="panel-body text-center">
 	                        <h4 class="panel-header">
 	                        	<a href="">
@@ -48,9 +65,37 @@
 	                        </div>
 	                    </div>
                 	</div>
-				</div>
+				</div> -->
 			
 			@endforeach
+			</section>
 		</div>
-	</div>
+		<script>
+			(function() {
+	
+				function init() {
+					var speed = 300,
+						easing = mina.backout;
+
+					[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+						var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+							pathConfig = {
+								from : path.attr( 'd' ),
+								to : el.getAttribute( 'data-path-hover' )
+							};
+
+						el.addEventListener( 'mouseenter', function() {
+							path.animate( { 'path' : pathConfig.to }, speed, easing );
+						} );
+
+						el.addEventListener( 'mouseleave', function() {
+							path.animate( { 'path' : pathConfig.from }, speed, easing );
+						} );
+					} );
+				}
+
+				init();
+
+			})();
+		</script>
 @stop
