396cc95fafd3dc78d09a35356d385bc3f751f26b
jorgegonzac <jorgegonzac@gmail.com>
2015-04-04 22:20:06 -0600

    Hot Fix [admin]
    Admin layout was including headBooking into its layout (it should
    include head instead). I fixed this error so the heads are separated
    (different views with different needs need different heads).

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -16,16 +16,20 @@
 <!-- JS are placed here -->
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
 
 {{ HTML::script('js/hovers.js') }}
 {{ HTML::script('js/snap.svg-min.js') }}
+{{ HTML::script('js/jquery.dataTables.min.js') }}
 {{ HTML::script('js/bootstrap.js') }}
-
-
+{{ HTML::script('js/bootstrap.min.js') }}
 
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <script>
+	$(document).ready(function(){
+	    $('#myTable').dataTable();
+	});
 	$(document).ready(function(){
 		$('#selectLaboratory').on('change', function(){
 			var id = $(this).val();
