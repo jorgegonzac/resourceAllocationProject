13fba162f2dc7807271059b7f34ccd0f7ff71c03
jorgegonzac <jorgegonzac@gmail.com>
2015-04-04 22:10:31 -0600

    Solve conflicts
    Solve conflicts with headBooking, I deleted jqueryDatatables, I
    considered that it wasn’t used on this view.

--- app/views/includes/headBooking.blade.php
+++ app/views/includes/headBooking.blade.php
@@ -11,30 +11,14 @@
 {{ HTML::style('css/styles.css')}}
 {{ HTML::style('css/material-fullpalette.css')}}
 {{ HTML::style('css/material.css')}}
-<link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"></style>
 
 <!-- JS are placed here -->
-<<<<<<< HEAD
-<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-
-{{ HTML::script('js/jquery.dataTables.min.js') }}
-{{ HTML::script('js/bootstrap.js') }}
-{{ HTML::script('js/bootstrap.min.js') }}
-
-<!-- <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-
-<script>
-$(document).ready(function(){
-    $('#myTable').dataTable();
-});
-=======
 
 {{ HTML::script('js/hovers.js') }}
 {{ HTML::script('js/snap.svg-min.js') }}
+{{ HTML::script('js/jquery.dataTables.min.js') }}
+{{ HTML::script('js/bootstrap.js') }}
+{{ HTML::script('js/bootstrap.min.js') }}
 
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
@@ -273,6 +257,4 @@ $(document).ready(function(){
 			});	
 		});
 	});
-
->>>>>>> User-BookingResource
 </script>
