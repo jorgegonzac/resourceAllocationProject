dc2f927caab790d3edb2cbd2b66c937c03627d37
jorgegonzac <jorgegonzac@gmail.com>
2015-04-23 17:11:31 -0500

    Feature: Notify booking to user
    This  feature was finished. New views were added in order to make a
    better project structure. Mail functionality of laraval was used. The
    credencials for the email are:
    resourceallocationitesm@gmail.com / ResourcesITESM

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'resourceallocationitesm@gmail.com', 'name' => 'Resource Allocation'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'resourceallocationitesm@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'ResourcesITESM',
 
 	/*
 	|--------------------------------------------------------------------------
