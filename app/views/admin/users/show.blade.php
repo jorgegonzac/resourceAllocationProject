8af54a2346ab6fe87dd08e20e15ad665609d3797
andyMonterrubio <a01205154@itesm.mx>
2015-04-26 22:45:02 -0500

    View show user

--- app/views/admin/users/show.blade.php
+++ app/views/admin/users/show.blade.php
@@ -1,26 +1,52 @@
 @extends('layouts.adminlayout')
 @section('content')
+{{ HTML::style('css/style_show.css')}}
 
-<div class="text-center">
-    <h3>Mostrando información de </h3><h1>{{ $user->school_id }}</h1>
 
-    <div class="jumbotron text-center">
-        <p>
-            <strong>Nombre:</strong> {{ $user->first_name   }}<br>
-            <strong>Apellido paterno:</strong> {{ $user->first_last_name }}<br>
-            <strong>Apellido materno:</strong> {{ $user->second_last_name }}<br>
-            <strong>Correo:</strong> {{ $user->email1 }}<br>
-            <strong>Correo alternativo:</strong> {{ $user->email2 }}<br>
-            <strong>School id:</strong> {{ $user->school_id }}<br>
-            <strong>Career:</strong> {{ $user->career }}
-        </p>
+<div class="form" style="width:800px; margin:0 auto;">
+
+    <div class="text-center">
+        <h3>Información de {{ $user->school_id }}</h3>
     </div>
+    <div class="sep"></div>
 
+    <table class='table borderless' >
+        <tr>
+            <td><strong>Nombre:</strong> </td>
+            <td>{{ $user->first_name   }}</td>
+        </tr>
+        <tr>
+            <td><strong>Apellido paterno:</strong> </td>
+            <td>{{ $user->first_last_name }}</td>
+        </tr>
+        <tr>
+            <td><strong>Apellido materno:</strong> </td>
+            <td>{{ $user->second_last_name }}</td>
+        </tr>
+        <tr>
+            <td><strong>Correo:</strong> </td>
+            <td>{{ $user->email1 }}</td>
+        </tr>
+        <tr>
+            <td><strong>Correo alternativo:</strong></td>
+            <td>{{ $user->email2 }}<br></td>
+        </tr>
+        <tr>
+            <td><strong>Matricula:</strong> </td>
+            <td>{{ $user->school_id }}<br></td>
+        </tr>
+        <tr>
+            <td><strong>Carrera:</strong> </td>
+            <td>{{ $user->career }}</td>
+        </tr>
+    </table>
+
+    <div class="text-center">
     <form action="{{ URL::to('users') }}">
             <button type="submit" class="btn btn-info"> Regresar</button>
     </form>
 
-
+    </div>
 </div>
 
 @stop
