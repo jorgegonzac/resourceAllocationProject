6d9b0f89fcae051a775fded2d012110786c99429
andyMonterrubio <a01205154@itesm.mx>
2015-04-26 19:40:15 -0500

    View create user completed

--- app/views/admin/users/create.blade.php
+++ app/views/admin/users/create.blade.php
@@ -1,64 +1,83 @@
 @extends('layouts.adminlayout')
 @section('content')
+{{ HTML::style('css/style_form.css')}}
 
-<div class="text-center">
 
-	<h2> Crear Nuevo Usuario</h2>
+<div class="form" style="width:800px; margin:0 auto;">
+
+	<div class="text-center">
+		<h2> Crear Nuevo Usuario</h2>
+	</div>
+	<div class="sep"></div>
 
 	{{ Form::open(array('route' => 'users.store')) }}
 
-		<div class="form-group">
-			{{ Form::label('first_name', 'Nombre:') }}
-			{{ Form::text('first_name') }}
+
+	<div class="container-form">
+		<div class="inputs">
+			<div class="form-group">
+				{{ Form::label('first_name', 'Nombre:&nbsp;') }}
+				{{ Form::text('first_name') }}
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('first_last_name', 'Apellido Paterno: &nbsp;') }}
+				{{ Form::text('first_last_name') }}
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('second_last_name', 'Apellido Materno:&nbsp; ') }}
+				{{ Form::text('second_last_name') }}
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('email1', 'Correo:&nbsp; ') }}
+				{{ Form::text('email1') }}
+				<div class="errors">
+					{{ $errors->first('email1', '<span class="error">:message</span>') }}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('email2', 'Correo alternativo:&nbsp;') }}
+				{{ Form::text('email2') }}
+				<div class="errors">
+					{{ $errors->first('email2', '<span class="error">:message</span>') }}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('password', 'Contraseña:&nbsp; ') }}
+				{{ Form::text('password') }}
+
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('school_id', 'Matrícula:&nbsp; ') }}
+				{{ Form::text('school_id') }}
+				<div class="errors">
+					{{ $errors->first('school_id', '<span class="error">:message</span>') }}
+				</div>
+			</div>
+
+			<div class="form-group">
+				{{ Form::label('career', 'Carrera:&nbsp;') }}
+				{{ Form::text('career') }}
+			</div>
+		
 		</div>
+			
+			<div class="text-center">
+			    {{ Form::button('Crear',array('type' => 'submit', 'class' => 'btn btn-success btn-sm ')) }}
+				{{ Form::close() }}
 
-		<div class="form-group">
-			{{ Form::label('first_last_name', 'Apellido Paterno:') }}
-			{{ Form::text('first_last_name') }}
-		</div>
+				<form action="{{ URL::to('users') }}">
+			        	<button type="submit" class="btn btn-danger btn-sm "> Cancelar</button>
+				</form>
+			</div>
+	</div>
 
-		<div class="form-group">
-			{{ Form::label('second_last_name', 'Apellido Materno:') }}
-			{{ Form::text('second_last_name') }}
-		</div>
-
-		<div class="form-group">
-			{{ Form::label('email1', 'Correo:') }}
-			{{ Form::text('email1') }}
-			{{ $errors->first('email1', '<span class="error">:message</span>') }}
-		</div>
-
-		<div class="form-group">
-			{{ Form::label('email2', 'Correo alternativo:') }}
-			{{ Form::text('email2') }}
-			{{ $errors->first('email2', '<span class="error">:message</span>') }}
-		</div>
-
-		<div class="form-group">
-			{{ Form::label('password', 'Contraseña:') }}
-			{{ Form::text('password') }}
-		</div>
-
-		<div class="form-group">
-			{{ Form::label('school_id', 'Matrícula:') }}
-			{{ Form::text('school_id') }}
-			{{ $errors->first('school_id', '<span class="error">:message</span>') }}
-		</div>
-
-		<div class="form-group">
-			{{ Form::label('career', 'Carrera:') }}
-			{{ Form::text('career') }}
-		</div>
-
-		<div class="text-center">
-		    {{ Form::button('Crear',array('type' => 'submit', 'class' => 'btn btn-success')) }}
-		</div>
-	{{ Form::close() }}
-
-
-		<form action="{{ URL::to('users') }}">
-	        	<button type="submit" class="btn btn-danger"> Cancelar</button>
-		</form>
 
 </div>
+
 @stop
