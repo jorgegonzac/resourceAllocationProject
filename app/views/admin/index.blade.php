3060182b4824743c1f6261abeb97e77330363f48
a01320622 <a01320622@itesm.mx>
2015-05-11 17:25:51 -0500

    Finished lab admin graphs
    all graphs are finished for specific lab admin

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -1,67 +1,149 @@
 @extends('layouts.adminlayout')
 @section('content')
+<div margin="30">
+<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
 
-<style>
-.cont{
-
-
-	padding-left:300px;
-}
-
-</style>
-<div class="cont"><h2>Bienvenido</h2> 
+	<div class="row">
+		<div class="col-xs-6 col-sm-6 col-md-6"> <h2>Bienvenido</h2> </div>
+	</div>
 	@if(Session::get('super')==1)
 		<h4>Super Usuario</h4>
+
 	@else
+
 		<h4>Administrador del {{ Session::get('lab_name') }} </h4>
-		@foreach( $resource as $r)
 
-			<p>{{$r}}</p>		
-			
-		@endforeach
-		@foreach( $total as $t)
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<h3 align="center">Recursos más usados</h3>
+		  	<div class="sample shadow-z-2">
+		  		
+		  		<canvas id = "mostUsed" ></canvas>
+		  	</div>
+		 </div>
 
-			<p>{{$t}}</p>		
-			
-		@endforeach
+
+	
+		  <br><br>
+
+		  
+		  <div class="row">
+		  <div class="col-xs-12">
+		  	<h3 align="center">Usuarios más activos</h3>
+		  	<div class="sample shadow-z-2">
+		  	
+		  		<canvas id = "mostUser"  ></canvas>
+		  	</div>
+		  </div>
+		</div>
+
+		<br><br>
+
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<h3 align="center">Reservaciones por mes</h3>
+		  	<div class="sample shadow-z-2">
+		  		
+		  		<canvas id = "mostMonth"></canvas>
+		  	</div>
+		  </div>
+		 
+		</div>
+
+		<br><br>
+
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<h3 align="center">Reservaciones por día</h3>
+		  	<div class="sample shadow-z-2">
+		  		
+		  		<canvas id = "mostDay"></canvas>
+		  	</div>
+		  </div>
+		 
+		</div>
+		
+		
+		<script >
+
+			(function(){
+				var ctx = document.getElementById('mostUsed').getContext('2d');
+				var ctx1 = document.getElementById('mostUser').getContext('2d');
+				var ctx2 = document.getElementById('mostMonth').getContext('2d');
+				var ctx3 = document.getElementById('mostDay').getContext('2d');
+
+				Chart.defaults.global.responsive = true;
+
+				var chartResources = {
+
+					labels:{{json_encode($resource)}},
+
+					datasets:[{
+
+						data:{{json_encode($total)}}
+
+					}]
+
+				};
+
+				var chartUsers = {
+
+					labels:{{json_encode($userName)}},
+
+					datasets:[{
+
+						data:{{json_encode($userCount)}}
+
+					}]
+
+				};
+
+				var chartMonth = {
+
+					labels: {{json_encode($month)}},
+
+					datasets:[{
+
+						data:{{json_encode($monthCount)}}
+
+					}]
+
+				};
+
+				var chartDay = {
+
+					labels: ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"],
+
+					datasets:[{
+
+						data:{{json_encode($dayCount)}}
+
+					}]
+
+				};
+
+				new Chart(ctx).Bar(chartResources);
+				new Chart(ctx1).Bar(chartUsers);
+				new Chart(ctx2).Line(chartMonth);
+				new Chart(ctx3).Bar(chartDay);
+
+				
+
+
+			})();
+
+
+		</script>
+
 	@endif
 
-	<canvas id = "mostUsed" width = "600" height="300" ></canvas>
+	
+</div>
 
 </div>
 
 
 
-<script >
-
-	(function(){
-		var ctx = document.getElementById('mostUsed').getContext('2d');
-
-		var chart = {
-
-			labels:{{json_encode($resource)}},
-
-			datasets:[{
-
-				data:{{json_encode($total)}}
-
-
-			}]
-
-
-
-		};
-
-
-		new Chart(ctx).Bar(chart);
-
-	})();
-
-
-
-
-
-</script>
 
 @stop
 
--- storedProcedure.sql
+++ storedProcedure.sql
@@ -1,3 +1,4 @@
+-- by RESOURCE
 DELIMITER //
  CREATE PROCEDURE ResourceTotal(IN lab_id INT(10))
    BEGIN
@@ -14,3 +15,76 @@ DELIMITER //
 	group by resources.name;
    END //
  DELIMITER ;
+
+-- by USER
+
+ DELIMITER //
+ CREATE PROCEDURE UserTotal(IN lab_id INT(10))
+   BEGIN
+   DECLARE curMonth INT;
+	DECLARE curYear INT;
+	SET curMonth = (select MONTH(NOW()));
+	SET curYear = (select YEAR(NOW()));
+	select bookings.user_id,count(resources.id) as total 
+	from resources,bookings
+	where bookings.resource_id = resources.id 
+	AND resources.laboratory_id = lab_id
+	AND MONTH(bookings.start_date) = curMonth
+	AND YEAR(bookings.start_date) = curYear
+	group by bookings.user_id;
+	
+   END //
+ DELIMITER ;
+
+-- by MONTH
+DELIMITER //
+ CREATE PROCEDURE MonthTotal(IN lab_id INT(10))
+   BEGIN
+   	DECLARE curYear INT;
+   	SET curYear = (select YEAR(NOW()));
+	select MONTH(bookings.start_date) as month ,count(bookings.id) as total 
+	from resources,bookings
+	where bookings.resource_id = resources.id 
+	AND resources.laboratory_id = lab_id
+	AND YEAR(bookings.start_date) = curYear
+	group by MONTH(bookings.start_date);
+	
+   END //
+ DELIMITER ;
+
+-- by WEEK
+ DELIMITER //
+ CREATE PROCEDURE WeekTotal(IN lab_id INT(10))
+   BEGIN
+   	DECLARE curMonth INT;
+	DECLARE curYear INT;
+	SET curMonth = (select MONTH(NOW()));
+	SET curYear = (select YEAR(NOW()));
+	select DAYOFWEEK(bookings.start_date) as day ,count(bookings.id) as total 
+	from resources,bookings
+	where bookings.resource_id = resources.id
+    AND resources.laboratory_id = lab_id
+    AND MONTH(bookings.start_date) = curMonth
+    AND YEAR(bookings.start_date) = curYear
+	group by DAYOFWEEK(bookings.start_date);
+	
+   END //
+ DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
